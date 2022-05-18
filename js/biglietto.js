09da4fe2c064c820525f61f34a7a14b8e050dd02
javoxpl <liacovone7@outlook.it>
2022-05-18 16:04:57 +0200

    completato

--- js/biglietto.js
+++ js/biglietto.js
@@ -21,12 +21,74 @@ posso inserire un minimo / massimo di km possibili?
 */
 
 
-/*
-Passaggi:
-- fare questo
-- fare quest'altro
-SE succede questo
-faccio questo
-Altrimenti
-*/
 
+const prezzoKm = 0.21;
+let sconto20 
+let sconto40
+
+const messaggioEtaErrata = "L'èta inserita non è valida, inserire un numero";
+const messaggioKmErrati = "La tratta inserita non è valida, inserire un numero";
+const messagioInserisciDato = "Valore non dichiarato";
+
+let eta;
+let km;
+let prezzo;
+
+eta = prompt("Inserisci l'età del viaggiatore");
+
+if ((eta < 0) || (isNaN(eta)) || (eta === " ")){
+    alert(messagioInserisciDato);
+    eta = messagioInserisciDato;
+}
+
+km = prompt("Inserire i km da percorrere");
+
+if ((km < 0) || (isNaN(km)) || (km === " ")) {
+    alert(messaggioKmErrati)
+    km = messagioInserisciDato;
+}
+
+ prezzo = prezzoKm * km;
+
+if (eta <= 18) {
+    sconto20 = (prezzo * 20) / 100;
+    prezzo = prezzo - sconto20
+} else if (eta >= 65){
+        sconto40 = (prezzo * 40) / 100;
+        prezzo = prezzo - sconto40
+} else {
+    prezzo = prezzo
+}
+
+
+
+const spanEtaUtente = document.getElementById('span-eta-utente');
+
+if (eta === messagioInserisciDato){
+    spanEtaUtente.innerHTML = eta
+} else {
+    spanEtaUtente.innerHTML = eta + " anni"
+}
+
+const spanKm = document.getElementById('span-km');
+
+if (prezzoKm === messagioInserisciDato){
+    spanKm.innerHTML = prezzoKm
+} else {
+    spanKm.innerHTML = prezzoKm + " km"
+}
+
+const spanPrezzo = document.getElementById('span-prezzo');
+
+if (eta === messagioInserisciDato || prezzoKm === messagioInserisciDato){
+    spanPrezzo.innerHTML = "Impossibile calcolare il prezzo"
+} else {
+    spanPrezzo.innerHTML = prezzo.toFixed(2) + " €";
+}
+
+
+
+
+console.log(eta + "eta")
+console.log(prezzoKm + "km")
+console.log("prezzo" + prezzo)
